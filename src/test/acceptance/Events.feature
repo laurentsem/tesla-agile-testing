4dcc9b6e4782abe8803f603d96e8e91afe151739
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2021-01-14 13:59:46 +0100

    CA n°2(tout)

--- src/test/acceptance/Events.feature
+++ src/test/acceptance/Events.feature
@@ -1,6 +1,11 @@
 Feature: Evenements Tesla
   Scenario: je vais sur la page evenements
     Given je suis sur la page d'accueil
-    When je clique sur le menu burger
+    When je clique sur le menu
     And je clique sur l'onglet "Événements"
     Then je suis sur la page evenements
+
+  Scenario: Vérification du nombre d'événements
+    Given je suis sur la page events
+    Then je choisi une ville
+    And je vérifie s'il y a 15 evenements
--- src/test/acceptance/EventsStep.java
+++ src/test/acceptance/EventsStep.java
@@ -6,6 +6,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -34,30 +35,88 @@ public class EventsStep {
         // driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
-
-    @When("^je clique sur l'onglet \"([^\"]*)\"$")
-    public void je_clique_sur_l_onglet(String arg1) throws Throwable {
+    @When("^je clique sur le menu$")
+    public void je_clique_sur_le_menu() throws Throwable {
         driver.get("https://www.tesla.com/fr_FR/");
         Actions action = new Actions(driver);
         WebElement burgerMenu = driver.findElement(By.className("tds-menu-header-main--trigger_icon"));
         action.click(burgerMenu);
         action.build().perform();
+    }
+
+    @When("^je clique sur l'onglet \"([^\"]*)\"$")
+    public void je_clique_sur_l_onglet(String arg1) throws Throwable {
         Actions action2 = new Actions(driver);
         WebElement navEvenements = driver.findElement(By.xpath("/html/body/div[1]/div/header/div/div/nav/div[1]/nav[1]/ol/li[10]/a"));
-        System.out.println("aa------ = " + navEvenements.getText());
-        assertEquals(navEvenements.getText(), arg1);
-        action2.click(navEvenements);
-        action2.build().perform();
+        System.out.println("aa------ = " + navEvenements.getAttribute("innerHTML"));
+        assertEquals(arg1,navEvenements.getAttribute("innerHTML"));
+        try{
+            Thread.sleep(1000);
+            action2.click(navEvenements);
+            action2.build().perform();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Then("^je suis sur la page evenements$")
     public void je_suis_sur_la_page_evenements() throws Throwable {
-        String currentUrl = driver.getCurrentUrl();
-        assertEquals(currentUrl, "https://www.tesla.com/fr_FR/events");
+        try{
+            Thread.sleep(1000);
+            String currentUrl = driver.getCurrentUrl();
+            assertEquals( "https://www.tesla.com/fr_FR/events",currentUrl);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
+    @Then("^je suis sur la page events$")
+    public void je_suis_sur_la_page_events() throws Throwable {
+        driver.get("https://www.tesla.com/fr_FR/events");
+    }
+    @Then("^je choisi une ville$")
+    public void je_choisi_une_ville() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement inputCity = driver.findElement(By.id("edit-geoautocomplete"));
+        try{
+            Thread.sleep(5000);
+            Actions actions = new Actions(driver);
+            actions.sendKeys(inputCity,"Hong Kong");
+            actions.build().perform();
+            Thread.sleep(1000);
+            Actions actions1 = new Actions(driver);
+            actions1.sendKeys(Keys.RETURN);
+            actions1.build().perform();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Then("^je vérifie s'il y a (\\d+) evenements$")
+    public void je_vérifie_s_il_y_a_evenements(int arg1) throws Throwable {
+        WebElement content = driver.findElement(By.className("view-content"));
+        List<WebElement> list = content.findElements(By.tagName("div"));
+        int count = 0;
+        for(WebElement e : list){
+            if(e.getAttribute("class").contains("views-row"))
+            count++;
+        }
+        assertEquals(arg1,count);
+    }
+
+
+
 
     @After
     public void afterScenario() {
-        driver.quit();
+        try{
+            Thread.sleep(5000);
+            driver.quit();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
